d1dd69e5fb81f9679f5faa666e31d59e41b1627e
Satabari <satabari.mukherjee@gmail.com>
2021-06-09 11:48:55 +0530

    Second test case passed

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,7 +3,11 @@ public class Calculator {
 
 	public int calString(String numbers) {
 		// TODO Auto-generated method stub
-		return 0;
+		if(numbers.isEmpty())
+		{
+			return 0;
+		}
+		return Integer.parseInt(numbers);		
 	}
 	
 }
