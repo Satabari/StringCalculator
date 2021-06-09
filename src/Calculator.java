5006e40b7940afe562b51c425a4bbc8869ecd6f6
Satabari <satabari.mukherjee@gmail.com>
2021-06-09 12:36:20 +0530

    Adding the Refractor

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,13 +1,21 @@
 
 public class Calculator {
 
-	public int calString(String numbers) {
+	public int calString(String inputString) {
 		// TODO Auto-generated method stub
-		if(numbers.isEmpty())
+		if(isEmpty(inputString))
 		{
 			return 0;
 		}
-		return Integer.parseInt(numbers);		
+		return stringToInteger(inputString);
 	}
 	
+	private boolean isEmpty(String inputString) {
+		return inputString.isEmpty();
+	}
+	
+	
+	private int stringToInteger(String inputString) {
+		return Integer.parseInt(inputString);
+	}
 }
