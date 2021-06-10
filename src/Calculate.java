724c6752f3c26d1884d0e6df2cc9bbf03b658da3
Satabari <satabari.mukherjee@gmail.com>
2021-06-10 10:27:15 +0530

    Adding comments to the code

--- src/Calculate.java
+++ src/Calculate.java
@@ -5,6 +5,7 @@ import static org.testng.Assert.assertEquals;
 @Test
 public class Calculate {
 	
+	//constructor
 	private Calculator cal;
 	
 	@BeforeTest
@@ -12,37 +13,46 @@ public class Calculate {
 		cal = new Calculator();
 	}
 	
+	//testing for no input
 	public void emptyString() throws Exception
 	{
 		//Calculator cal = new Calculator();
 		assertEquals(cal.calString(""),0);
 	}
+	
+	//testing for single number input
 	public void singleNumber() throws Exception
 	{
 		assertEquals(cal.calString("1"),1);
 	}
 	
+	//testing for two numbers input separated by commas
 	public void twoNumbers() throws Exception{
 		assertEquals(cal.calString("1,2"),3);
 	}
 	
+	//testing for two numbers input separated by new line
 	public void twoNumbersDelimitedByNewLine() throws Exception{
 		assertEquals(cal.calString("1\n2"),3);
 	}
 	
+	//testing for three no.s input
 	public void threeNumbers() throws Exception{
 		assertEquals(cal.calString("1\n2,3"),6);
 	}
 	
+	//testing for negative number exception
 	@Test(expectedExceptions = Exception.class)
 	public void negativeInput() throws Exception{
 		cal.calString("-1");
 	}
 	
+	//testing for bigger than 1000 as input
 	public void ignoreBiggerNumbers() throws Exception{
 		assertEquals(cal.calString("20,950,1010"),970);
 	}
 	
+	//testing for input where "//" will be ignored as delimiter
 	public void multipleDelimiter() throws Exception{
 		assertEquals(cal.calString("//;\\n1;2"),3);
 	}
--- src/Calculator.java
+++ src/Calculator.java
@@ -1,35 +1,44 @@
 
 public class Calculator {
 
+	//regular expression to check
 	private final String regEx = "[^0-9]+";
 	
+	//Addition of String method
 	public int calString(String inputString) throws Exception{
 		
+		//Splitting the String for regular expression
 		String[] numbers = inputString.split(regEx);
 		
+		//for empty string input
 		if(isEmpty(inputString))
 		{
 			return 0;
 		}
 		
+		//for single number input
 		else if(inputString.length() == 1){
 			return stringToInteger(inputString);
 		}
 		
+		//for non single number inputs
 		else {
 			return sumInt(numbers);
 		}
 		
 	}
 	
+	//for checking negative number input
 	private int sumInt(String[] numbers) throws Exception{		
 		negNumInput(numbers);		
 		return sumIntLessThan1000(numbers);
 	}
 	
+	//adding the numbers in String inputs
 	private int sumIntLessThan1000(String[] numbers) {
 		int sum = 0;
 		for(String loop:numbers){
+			//for checking inputs greater than 1000
 			if(stringToInteger(loop) > 1000) {
 				continue;
 			}				
@@ -38,6 +47,7 @@ public class Calculator {
 		return sum;
 	}
 	
+	//creating exception for negative number inputs
 	private void negNumInput(String[] numbers) throws Exception{
 		for(String loop:numbers){
 			if(stringToInteger(loop) < 0) {
@@ -46,10 +56,12 @@ public class Calculator {
 		}
 	}
 	
+	//checking for empty string
 	private boolean isEmpty(String inputString) {
 		return inputString.isEmpty();
 	}	
 	
+	//parsing the numbers from String to Integers
 	private int stringToInteger(String inputString) {
 		return Integer.parseInt(inputString);
 	}
